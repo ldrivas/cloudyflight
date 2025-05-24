c05f4fb31d41b2be9c023eca7575d0679c4f3128
ldrivas <lucien.drivas@gmail.com>
2025-05-24 23:09:04 +0930

    test

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 module.exports = function (eleventyConfig) {
+  // Copy everything that isn't processed by Eleventy (e.g. images, CSS, JS)
+  eleventyConfig.addPassthroughCopy({ "src": "." });
+
   return {
     dir: {
-      input: "src",
-      output: "public",
-      includes: "_includes",
+      input: "src",     // Where your source files live
+      output: "public", // Where Eleventy puts the built site
     },
   };
 };
